3c6c75c1c12f1481569e6c561d91a91026aa41ca
Teodora Vela <teodora11vela@gmail.com>
2025-01-19 14:35:58 +0200

    added boost on space

--- Snow Boarder/Assets/Script/PlayerController.cs
+++ Snow Boarder/Assets/Script/PlayerController.cs
@@ -1,19 +1,39 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 
-        9f;
+    [SerializeField] float torqueAmount = 9f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 18f;
+
     Rigidbody2D rb;
-    CircleCollider2D spriteHead;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    SurfaceEffector2D surfaceEffector;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        SpeedUpPlayer();
+    }
+
+    private void SpeedUpPlayer()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            surfaceEffector.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.D))
         {
